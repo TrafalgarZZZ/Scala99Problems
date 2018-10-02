9c20cbccb951cf1b44b3219bead12988bc66b9d3
trafalgarzzz <trafalgarz@outlook.com>
2018-10-02 11:22:52 +0800

    p57

--- P54-P69/BinaryTree/Main.scala
+++ P54-P69/BinaryTree/Main.scala
@@ -5,5 +5,12 @@ object Main {
         //P56
         Console println Node('a', Node('b'), Node('c')).isSymmetric
         Console println Node('a', Node('b', End, Node('c')), Node('d')).isSymmetric
+
+        //p57
+        val testLs = List(3, 5, 7, 1, 2, 3, 6, -3, 10)
+        Console println BinarySearchTree.fromList(testLs)
+
+        val testLs2 = List("aaa", "abc", "ddd", "acc", "xcc", "123")
+        val x = BinarySearchTree.fromList(testLs2)
     }
 }
---
+++ P54-P69/BinaryTree/p57.scala
@@ -0,0 +1,31 @@
+package binarytree
+
+sealed abstract class BinarySearchTree[+T] {
+    def addValue[U >: T](x: U)(implicit ev$1: U => Ordered[U]): BinarySearchTree[U]
+}
+
+case class BSTNode[+T](value: T, left: BinarySearchTree[T], right: BinarySearchTree[T]) extends BinarySearchTree[T] {
+    override def toString: String = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+
+    override def addValue[U >: T](x: U)(implicit ev$1: U => Ordered[U]): BinarySearchTree[U] = {
+        if(x < value) BSTNode(value, left.addValue(x), right)
+        else BSTNode(value, left, right.addValue(x))
+    }
+}
+
+case object BSTEnd extends BinarySearchTree[Nothing] {
+    override def toString: String = "."
+
+    override def addValue[U >: Nothing](x: U)(implicit ev$1: U => Ordered[U]): BinarySearchTree[U] = BSTNode(x)
+}
+
+object BSTNode {
+    def apply[T](value: T, left: BinarySearchTree[T], right: BinarySearchTree[T]): BinarySearchTree[T] = new BSTNode(value, left, right)
+    def apply[T](value: T): BinarySearchTree[T] = new BSTNode[T](value, BSTEnd, BSTEnd)
+}
+
+object BinarySearchTree {
+    def fromList[T](ls: List[T])(implicit ev$1: T => Ordered[T]): BinarySearchTree[T] =
+        ls.foldLeft(BSTEnd: BinarySearchTree[T])(_.addValue(_))
+}
+
